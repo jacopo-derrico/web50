03992f4e0dc6faeb0a5ab943f65c315df0b84a3f
Jacopo D'Errico <derricoj.design@gmail.com>
2025-11-26 16:05:47 +0100

    feat: search route and redirest from search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -12,3 +12,20 @@ def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "entry": util.get_entry(title)
     })
+
+def search(request):
+    query = request.GET.get('q').lower()
+    all_entries = util.list_entries()
+    matching_entries = []
+
+    for r in all_entries:
+        if r.lower() == query:
+            return redirect('entry', title=r)
+        elif query in r.lower():
+            matching_entries.append(r)
+
+    return render(request, "encyclopedia/search.html", {
+            "results": matching_entries,
+            "query" : query
+    })
+        
