1d88dd3676ad5531b479b4699e137b3ecfeab403
Jacopo D'Errico <derricoj.design@gmail.com>
2025-11-26 12:23:00 +0100

    feat: route to entries

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,3 +7,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title)
+    })
