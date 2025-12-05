8104e0c5f40bacac4a0e7ab2af2cf61cc9a01fdb
jacopo-derrico <derricoj.design@gmail.com>
2025-12-05 12:07:59 +0100

    feat: new models and import to admin user

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Auctions, Bids, Comments, Categories
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register([Auctions, Bids, Comments, Categories])
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,4 +3,26 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField('Auctions', related_name="watch_auction", blank=True)
+
+# auctions model
+class Auctions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Bids(models.Model):
+    bid = models.IntegerField()
+    users = models.ManyToManyField(User, related_name="bid_user")
+    auctions = models.ManyToManyField(Auctions, related_name="bid_auction")
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=250)
+    users = models.ManyToManyField(User, related_name="comment_user")
+    auctions = models.ForeignKey(Auctions, on_delete=models.CASCADE)
+
+class Categories(models.Model):
+    title = models.CharField(max_length=30)
